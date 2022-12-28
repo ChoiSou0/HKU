da7f6807c6ec6ea17d053a1f6121c00d67115e1e
ChoiSou0 <95004780+ChoiSou0@users.noreply.github.com>
2022-12-28 18:18:11 +0900

    클리어 조건 수정중

--- Assets/Sou0_Choi/StageClear.cs
+++ Assets/Sou0_Choi/StageClear.cs
@@ -12,6 +12,8 @@ public class Clear
 public class StageClear : MonoBehaviour
 {
     [SerializeField] private List<Clear> ClearList = new List<Clear>();
+    [SerializeField] private int MaxComplete;
+    [SerializeField] private int Complete;
 
     // Start is called before the first frame update
     void Start()
@@ -20,8 +22,27 @@ public class StageClear : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
-        
+        Clear();
+    }
+
+    protected void Clear()
+    {
+        for (int i = 0; i < ClearList.Count; i++)
+        {
+            if (ClearList[i].ClearTile.gameObject.transform.rotation.z == ClearList[i].ClearRotation)
+            {
+                Complete++;
+            }
+            else
+            {
+                if (Complete > 0)
+                    Complete--;
+            }
+        }
+
+        if (Complete == MaxComplete)
+            Debug.Log("Clear");
     }
 }
--- Assets/Sou0_Choi/TestStage.cs
+++ Assets/Sou0_Choi/TestStage.cs
@@ -13,6 +13,6 @@ public class TestStage : StageClear
     // Update is called once per frame
     void Update()
     {
-        
+        base.Update();
     }
 }
