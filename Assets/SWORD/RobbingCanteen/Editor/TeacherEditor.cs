d6b2a1abe53e602ab95396be776d45c7a3037352
576rick1412 <576rick1412@gmail.com>
2022-12-29 23:48:29 +0900

    TMP 추가 , 매점털기게임 기능 추가
    매점 털기 게임
    선생님이 뒤돌기 전 머리 위에 경고 표시가 깜빡이도록 추가함
    로직 개선
    인스펙터 보기 편하게 재배치

--- Assets/SWORD/RobbingCanteen/Editor/TeacherEditor.cs
+++ Assets/SWORD/RobbingCanteen/Editor/TeacherEditor.cs
@@ -6,8 +6,7 @@ public class TeacherEditor : Editor
 {
     Teacher_CS teacher;
 
-    SerializedProperty RandTime_Set_Prop;
-    SerializedProperty ActiveTime_Prop;
+    SerializedProperty Setting_Prop;
 
     private void OnEnable()
     {
@@ -15,30 +14,32 @@ public class TeacherEditor : Editor
     }
     private void Awake()
     {
-        RandTime_Set_Prop = serializedObject.FindProperty("RandTime_Set");
-        ActiveTime_Prop = serializedObject.FindProperty("Active_Time");
+        Setting_Prop = serializedObject.FindProperty("setting");
     }
 
     public override void OnInspectorGUI()
     {
-        EditorGUILayout.PropertyField(RandTime_Set_Prop);
-        EditorGUILayout.PropertyField(ActiveTime_Prop);
 
-        if(RandTime_Set_Prop.boolValue)
+        EditorGUILayout.PropertyField(Setting_Prop);
+
+        if((Teacher_CS.Setting)  Setting_Prop.enumValueIndex == Teacher_CS.Setting.None)
+        { }
+        if ((Teacher_CS.Setting) Setting_Prop.enumValueIndex == Teacher_CS.Setting.Rotate_Cast)
         {
             EditorGUILayout.BeginHorizontal();
-
             EditorGUILayout.PrefixLabel("랜덤 범위 ( 최소 / 최대 )");
-            teacher.min_Time = EditorGUILayout.FloatField(teacher.min_Time);
-            teacher.max_Time = EditorGUILayout.FloatField(teacher.max_Time);
-
+            teacher.min_Time = EditorGUILayout.FloatField(teacher.min_Time, GUILayout.Width(100));
+            teacher.max_Time = EditorGUILayout.FloatField(teacher.max_Time, GUILayout.Width(100));
             EditorGUILayout.EndHorizontal();
-        }
 
-        if (ActiveTime_Prop.boolValue)
+
+            teacher.Trun_Time = EditorGUILayout.FloatField("돌아보는 간격", teacher.Trun_Time);
+            teacher.See_Time  = EditorGUILayout.FloatField("보고있는 시간", teacher.See_Time);
+        }
+        if ((Teacher_CS.Setting) Setting_Prop.enumValueIndex == Teacher_CS.Setting.Rotate_Warning)
         {
-            teacher.Trun_Time = EditorGUILayout.FloatField("돌아보는 간격", teacher.Trun_Time); 
-            teacher.See_Time  = EditorGUILayout.FloatField("보고있는 시간", teacher.See_Time); 
+            teacher.Warning_Times = EditorGUILayout.IntField  ("경고 반복 횟수", teacher.Warning_Times);
+            teacher.Warning_sec   = EditorGUILayout.FloatField("경고 시간",      teacher.Warning_sec  );
         }
 
         serializedObject.ApplyModifiedProperties();
